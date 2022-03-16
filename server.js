5994328c0165d9a89ff4c54b351c01fb21e6a45e
tedlopus <tedlopus@gmail.com>
2022-03-16 16:36:05 -0600

    added routes folder and server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
